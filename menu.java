0cb9aca0f9397dfc07a3c3beeeac892aa111d0e7
ThereseBulai <therese.bulai@gmail.com>
2024-11-11 14:35:11 +0100

    började programmera en meny

---
+++ menu.java
@@ -0,0 +1,24 @@
+public class menu {
+    public static void main(String[] args) {
+       
+       //Menyval för spelaren
+        System.out.println("Hej och välkommen till brädspelssamlingen TicTacToe");
+
+
+        System.out.println("Du kan välja på dessa spel");
+        System.out.println("1. Tre i rad");
+        System.out.println("2. Fyra i rad");
+        System.out.println("3. Fem i rad");
+
+
+            switch (args) {
+                case 1:
+                    ""
+                    break;
+            
+                default:
+                    break;
+            }
+
+    }
+}
